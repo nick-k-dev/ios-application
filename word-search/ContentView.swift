3eea476b54a16ce320777689d245327f10aa23d8
Nick Komarnicki <nickkomarnicki@gmail.com>
2020-09-04 16:15:57 -0400

    Initial Commit

---
+++ word-search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  word-search
+//
+//  Created by Nick on 2020-09-04.
+//  Copyright © 2020 Nick Komarnicki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
